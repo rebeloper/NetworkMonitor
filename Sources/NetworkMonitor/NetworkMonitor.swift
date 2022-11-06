a53949ef31fa6de4038f56f6439b4281953b3342
rebeloper <therebeloper@gmail.com>
2022-11-06 13:25:39 +0200

    first commit

--- Sources/NetworkMonitor/NetworkMonitor.swift
+++ Sources/NetworkMonitor/NetworkMonitor.swift
@@ -1,6 +1,20 @@
-public struct NetworkMonitor {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
+import Network
 
+public class NetworkMonitor: ObservableObject {
+    private let networkMonitor = NWPathMonitor()
+    private let workerQueue = DispatchQueue(label: "Monitor")
+    public var isConnected = false
+    
     public init() {
+        networkMonitor.pathUpdateHandler = { path in
+            self.isConnected = path.status == .satisfied
+            Task {
+                await MainActor.run {
+                    self.objectWillChange.send()
+                }
+            }
+        }
+        networkMonitor.start(queue: workerQueue)
     }
 }
--- Tests/NetworkMonitorTests/NetworkMonitorTests.swift
+++ Tests/NetworkMonitorTests/NetworkMonitorTests.swift
@@ -6,6 +6,5 @@ final class NetworkMonitorTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(NetworkMonitor().text, "Hello, World!")
     }
 }
